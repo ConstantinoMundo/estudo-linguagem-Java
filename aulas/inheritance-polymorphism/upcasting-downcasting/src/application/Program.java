db53c5f1467ee68b84c4101a6cd619431e5f60c3
ConstantinoMundo <constantinoszm03@gmail.com>
2023-10-10 07:53:19 +0100

    Downcasting Implementation

--- aulas/inheritance-polymorphism/upcasting-downcasting/src/application/Program.java
+++ aulas/inheritance-polymorphism/upcasting-downcasting/src/application/Program.java
@@ -11,11 +11,18 @@ public class Program {
 		Account account = new Account(20876, "Mundo", 20000.00);
 		BusinessAccount businessAccount = new BusinessAccount(01, "Mateus", 0.00, 5000.00);
 		SavingsAccount savingsAccount = new SavingsAccount(02, "Lucas", 0.00, 0.02);
-		
-		//Upcasting
+
+		// Upcasting
+
 		Account account1 = businessAccount;
 		Account account2 = new BusinessAccount(03, "Joz", 0.00, 1000.00);
 		Account account3 = savingsAccount;
+
+		// Downcasting
+
+		BusinessAccount businessAccount1 = (BusinessAccount) account;
+		businessAccount.loan(250.00);
+
 	}
 
 }
