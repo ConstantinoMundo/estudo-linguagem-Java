96da5fc44fdc11abc4f8c2d08e057410e6fba2ef
ConstantinoMundo <constantinoszm03@gmail.com>
2024-02-27 14:21:52 +0100

    Reading text file with classes File and Scanner

---
+++ aulas/working-with-files/file/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException ioException) {
+			System.out.println("Error: " + ioException.getMessage());
+		} finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
