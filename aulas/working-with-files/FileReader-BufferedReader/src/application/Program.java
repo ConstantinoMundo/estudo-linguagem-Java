93933896a88040d33cc73c7be3fe580dc0e1ece9
ConstantinoMundo <constantinoszm03@gmail.com>
2024-02-27 14:05:08 +0100

    try-with-resources block

--- aulas/working-with-files/FileReader-BufferedReader/src/application/Program.java
+++ aulas/working-with-files/FileReader-BufferedReader/src/application/Program.java
@@ -9,12 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "D:\\temp\\in.txt";
-		BufferedReader br = null;
-		FileReader fr = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			String line = br.readLine();
 
@@ -25,17 +21,6 @@ public class Program {
 			}
 		} catch (IOException ioException) {
 			System.out.println("Error: " + ioException.getMessage());
-		} finally {
-			try {
-				if(br != null) {
-					br.close();
-				}
-				if(br != null) {
-					br.close();
-				}
-			} catch (IOException ioException) {
-				ioException.printStackTrace();
-			}
 		}
 	}
 
